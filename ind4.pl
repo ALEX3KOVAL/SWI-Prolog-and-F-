ddd301bde876ee0518382a90ee2c39f86ec57b7d
ALEX3KOVAL <62327917+ALEX3KOVAL@users.noreply.github.com>
2020-04-22 20:06:21 +0300

    changes in ind4

---
+++ ind4.pl
@@ -0,0 +1,14 @@
+ind4:-Index is 0, N is 3,owner(Index,N,[хом€чок,кошка,собака]).
+owner(N,N,List):-!.
+owner(Index,N,List):-ind_access(Index,List,Pet),хоз€ин(Owner,Pet),write(Owner),write(' - хоз€ин '),write(Pet),nl,Index1 is Index+1,owner(Index1,N,List).
+ind_access(0,[Elem|_Tail],Elem):-!.
+ind_access(Index,_List,_Elem):-Index<0,!,fail.
+ind_access(Index,[_Head|Tail],Elem):-NextIndex is Index-1,ind_access(NextIndex,Tail,Elem).
+гул€ет(X,Y):-хоз€ин(X,Y).
+дружит(лена,Y):-гул€ет(Y,кошка).
+нет(пет€,кошка).
+одноподъездники(пет€,Y):-дружит(Y,C).
+одноподъездники(X,Y):-нет(Y,кошка).
+хоз€ин(тан€,кошка).
+хоз€ин(X,собака):-нет(X,кошка),одноподъездники(X,C),!.
+хоз€ин(X,хом€чок):-одноподъездники(X,пет€),дружит(X,—),!.
